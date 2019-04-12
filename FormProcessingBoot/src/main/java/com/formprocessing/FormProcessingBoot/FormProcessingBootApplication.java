e8f1032f2fdca8b50ed9209390929583ab46845a
Vishnu P <awevishnu@gmail.com>
2019-04-12 23:48:34 +0530

    Initial Commit

---
+++ FormProcessingBoot/src/main/java/com/formprocessing/FormProcessingBoot/FormProcessingBootApplication.java
@@ -0,0 +1,13 @@
+package com.formprocessing.FormProcessingBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormProcessingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormProcessingBootApplication.class, args);
+	}
+
+}
---
+++ FormProcessingBoot/src/test/java/com/formprocessing/FormProcessingBoot/FormProcessingBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.formprocessing.FormProcessingBoot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FormProcessingBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
